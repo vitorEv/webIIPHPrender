77f223335dca96a95ec11986f54e99e006f7c0e1
vitor <anubzs1414@gmail.com>
2026-09-17 10:27:08 -0300

    corrigindo formulario

--- cadastro.php
+++ cadastro.php
@@ -1,10 +1,15 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"]=="POST"){
-    $cadastro = $_POST["Cadastro"];
 
-    echo "Cadastro realizado com sucesso!<br>";
-    echo "Dado recebido: " .htmlspecialchars($cadastro);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $nome = $_POST["Nome"];
+    $email = $_POST["E-mail"];
+    $telefone = $_POST["Telefone"];
+
+    echo "Cadastro realizado com sucesso!<br><br>";
+    echo "Nome: " . htmlspecialchars($nome) . "<br>";
+    echo "E-mail: " . htmlspecialchars($email) . "<br>";
+    echo "Telefone: " . htmlspecialchars($telefone);
 }
-
 
 ?>
